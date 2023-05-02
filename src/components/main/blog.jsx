ed2eee02cff76423b293255ac4b699d7311fd26d
mohdamaan570 <mohdamaan570@gmail.com>
2023-05-02 19:42:35 +0530

    fix layout and add pagination

--- src/components/main/blog.jsx
+++ src/components/main/blog.jsx
@@ -11,18 +11,61 @@ const Main = () => {
     const dispatch = useDispatch();
     const state = useSelector((state) => state);
     const [fetchBlog, setFetchBlog] = useState(state);
-
+    
     useEffect(() => {
-        if(!state.blog.data){
+        if (!fetchBlog.blog.data) {
             dispatch(fetchBlogs());
         }
     }, [dispatch]);
-
+    
     useEffect(() => {
         setFetchBlog(state);
     }, [state]);
+    
+    // console.log(fetchBlog.blog.data?.article);
 
-    console.log(fetchBlog.blog.data?.article);
+
+    //Pagination(Start)
+    const [currentPage, setCurrentPage] = useState(1);
+    const blogsPerPage = 8;
+    const lastIndex = currentPage * blogsPerPage;
+    const firstIndex = lastIndex - blogsPerPage;
+    const records = fetchBlog.blog.data?.article.slice(firstIndex, lastIndex);
+    const numberOfPages = Math.ceil(fetchBlog.blog.data?.article.length / blogsPerPage);
+
+    const [isPrevButtonDisabled, setPrevButtonDisabled] = useState(false);
+    const [isNextButtonDisabled, setNextButtonDisabled] = useState(false);
+
+    useEffect(() => {
+        console.log(records);
+    }, []);
+
+    useEffect(()=>{
+        if (currentPage <= 1) {
+            setPrevButtonDisabled(true);
+        } else{
+            setPrevButtonDisabled(false);
+        }
+    })
+
+    useEffect(()=>{
+        if(currentPage >= numberOfPages){
+            setNextButtonDisabled(true);
+        } else {
+            setNextButtonDisabled(false);
+        }
+    })
+
+    const prevPages = () =>{
+            setCurrentPage(currentPage - 1);
+    }
+
+    const nextPages = () =>{
+            setCurrentPage(currentPage + 1);
+    }
+    //Pagination(End)
+
+
     if (state.blog.isLoading) {
         return <Loader />
     }
@@ -30,7 +73,7 @@ const Main = () => {
     return <div className='container'>
         <SideMenu />
         <div className="main">
-            {fetchBlog.blog.data?.article.map((data) => (
+            {records && records.map((data) => (
                 <div className="main-container" key={data._id}>
                     <div className="image-container">
                         <img className='image' src={data.articleImage[0]} alt="" />
@@ -43,6 +86,11 @@ const Main = () => {
                     <Link className="blog-link" to={`/blogDetail/${data._id}`}>Read more</Link>
                 </div>
             ))}
+        </div>
+        <div className="pages-button-container">
+            <button className="blog-previous-button" onClick={prevPages} disabled={isPrevButtonDisabled}>Previous</button>
+            <p className="blog-index-para">{currentPage} out of {numberOfPages}</p>
+            <button className="blog-next-button" onClick={nextPages} disabled={isNextButtonDisabled}>Next</button>
         </div>
     </div>
 }
